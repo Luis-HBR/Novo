cc6c888b627e4db4856469ac80c06c7250d2640d
Luis-HBR <luisrigotti3@gmail.com>
2026-04-13 20:57:46 -0300

    Update Aula11.py

--- Aula11.py
+++ Aula11.py
@@ -1,19 +1,21 @@
 import random
 import time
 
-lvl=int(1)
+lvl=int(0)
 id=int(0)
+up=[3,6,9,12,15,18,21,24,27,30]
 
 jogador = {
-    'vida':50*lvl,
-    'ataque':10*lvl,
-    'defesa':10*lvl
+    'vida':50,
+    'ataque':10,
+    'defesa':10,
+    'lvl':lvl
 }
 
 inimigo ={
-    'vida':50*lvl,
-    'ataque':10*lvl,
-    'defesa':10*lvl
+    'vida':50,
+    'ataque':10,
+    'defesa':10
 }
 
 
@@ -25,6 +27,7 @@ print('Jogador:')
 print(f'HP:{jogador['vida']}')
 print(f'Ataque:{jogador['ataque']}')
 print(f'Defesa:{jogador['defesa']}')
+print(f'Level:{lvl}')
 print('Inimigo:')
 print(f'HP:{inimigo['vida']}')
 print(f'Ataque:{inimigo['ataque']}')
@@ -38,6 +41,12 @@ while jogador['vida'] >= 1:
     vi=int(inimigo['vida'])
     di= max(1, int(inimigo['ataque']) - int(jogador['defesa']))
 
+    if id in up:
+        lvl=lvl+1
+        jogador['ataque']=jogador['ataque']+(2*lvl)
+        jogador['defesa']=jogador['defesa']+(2*lvl)
+        jogador['vida']=jogador['vida']+(10*lvl)
+
     print('Inimigo a vista')
 
     decisao = input('Atacar ou Fugir? a/f:')
@@ -48,36 +57,43 @@ while jogador['vida'] >= 1:
         print(f'HP:{jogador['vida']}')
         print(f'Ataque:{jogador['ataque']}')
         print(f'Defesa:{jogador['defesa']}')
+        print(f'Level:{lvl}')
         print('Inimigo:')
         print(f'HP:{inimigo['vida']}')
         print(f'Ataque:{inimigo['ataque']}')
         print(f'Defesa:{inimigo['defesa']}')
     elif decisao == 'f':
         print('fuga')
-        inimigo['vida']=50+random.randint(-5,5)
-        inimigo['ataque']=10+random.randint(-5,5)
-        inimigo['defesa']=10+random.randint(-5,5)
+        inimigo['vida']=50+(10*lvl)+random.randint(-5,5)
+        inimigo['ataque']=10+(2*lvl)+random.randint(-5,5)
+        inimigo['defesa']=10+(2*lvl)+random.randint(-5,5)
         print('Jogador:')
         print(f'HP:{jogador['vida']}')
         print(f'Ataque:{jogador['ataque']}')
         print(f'Defesa:{jogador['defesa']}')
+        print(f'Level:{lvl}')
         print('Inimigo:')
         print(f'HP:{inimigo['vida']}')
         print(f'Ataque:{inimigo['ataque']}')
         print(f'Defesa:{inimigo['defesa']}')
-    elif inimigo['vida']<=0:
+    elif inimigo['vida']<=1 and decisao == 'a':
         print('Inimigo Derrotado')
-        inimigo['vida']=50+random.randint(-5,5)
-        inimigo['ataque']=10+random.randint(-5,5)
-        inimigo['defesa']=10+random.randint(-5,5)
+        id=id+1
+        inimigo['vida']=50+(10*lvl)+random.randint(-5,5)
+        inimigo['ataque']=10+(2*lvl)+random.randint(-5,5)
+        inimigo['defesa']=10+(2*lvl)+random.randint(-5,5)
         jogador['vida']=jogador['vida']+15
         print('Jogador:')
         print(f'HP:{jogador['vida']}')
         print(f'Ataque:{jogador['ataque']}')
         print(f'Defesa:{jogador['defesa']}')
+        print(f'Level:{lvl}')
         print('Inimigo:')
         print(f'HP:{inimigo['vida']}')
         print(f'Ataque:{inimigo['ataque']}')
         print(f'Defesa:{inimigo['defesa']}')
+    elif jogador['lvl']:
+        print('Venceu')
+
 
 print('Game Ovo')
